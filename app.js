42efa9a2010c52d95b660f45da6087cdfbe111e4
EndRollModel <51425629+EndRollModel@users.noreply.github.com>
2021-10-24 05:26:41 +0800

    set header

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-let apiRouter = require('./api/index');
 let app = express();
 
 // view engine setup
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', apiRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
