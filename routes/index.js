f4e02db52d368d18eb429adaf55ea6fa9e1a7443
EndRollModel <51425629+EndRollModel@users.noreply.github.com>
2021-12-14 18:48:27 +0800

    add new music card

--- routes/index.js
+++ routes/index.js
@@ -51,4 +51,30 @@ router.get('/', async function (req, res) {
     }
 });
 
+router.get('/api/', async function (req, res) {
+    if (req.query.id === undefined && req.query.ids === undefined) return res.status(404).send().end(); // 沒有任何id或是ids請求
+    switch (true) {
+        case req.query.id !== undefined:
+        case req.query.ids !== undefined:
+            const id = req.query.id !== undefined ? req.query.id : req.query.ids;
+            const userdata = await dbModel.searchUser(id);
+            if (userdata !== undefined) {
+                const musicCard = await spotifyController.getMusicInfo(userdata);
+                if (req.query.id !== undefined) {
+                    res.setHeader('content-type', 'image/svg+xml')
+                    res.setHeader('Cache-Control', 'public,max-age=5,must-revalidate')
+                    return res.render('flexCard', {package: smallCard});
+                } else {
+                    res.setHeader('content-type', 'text/json')
+                    return res.send(JSON.stringify(musicCard))
+                }
+            } else {
+                console.log(`id or ids: "${req.query.id}" try `)
+                return res.status(404).send().end();
+            }
+        default:
+            break;
+    }
+});
+
 module.exports = router;
