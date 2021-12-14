1b41c86b00158cbbdbf8ff1f02ccb9b57d107b0f
EndrollModel <51425629+EndRollModel@users.noreply.github.com>
2021-12-14 21:59:19 +0800

    -

--- routes/index.js
+++ routes/index.js
@@ -77,4 +77,30 @@ router.get('/api/', async function (req, res) {
     }
 });
 
+router.get('/api/small/', async function (req, res) {
+    if (req.query.id === undefined && req.query.ids === undefined) return res.status(404).send().end(); // 沒有任何id或是ids請求
+    switch (true) {
+        case req.query.id !== undefined:
+        case req.query.ids !== undefined:
+            const id = req.query.id !== undefined ? req.query.id : req.query.ids;
+            const userdata = await dbModel.searchUser(id);
+            if (userdata !== undefined) {
+                const musicCard = await spotifyController.getMusicInfo(userdata);
+                if (req.query.id !== undefined) {
+                    res.setHeader('content-type', 'image/svg+xml')
+                    res.setHeader('Cache-Control', 'public,max-age=5,must-revalidate')
+                    return res.render('smallCardPlay', {package: musicCard});
+                } else {
+                    res.setHeader('content-type', 'text/json')
+                    return res.send(JSON.stringify(musicCard))
+                }
+            } else {
+                console.log(`id or ids: "${req.query.id}" try `)
+                return res.status(404).send().end();
+            }
+        default:
+            break;
+    }
+});
+
 module.exports = router;
